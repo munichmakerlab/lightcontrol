a3b7d0ed9fdbcd05c9816f2f2d9d51f62942b533
Severin Schols <severin@schols.de>
2014-02-19 23:11:46 +0100

    initial commit

---
+++ devices.js
@@ -0,0 +1,9 @@
+{
+	"prefix": "mumalab/room/lights/",
+	"devices": [
+		{ "name": "led_strip", "pretty_name": "LED Strip", "type": "switch" },
+		{ "name": "light1", "pretty_name": "Light 1", "type": "switch" },
+		{ "name": "light2", "pretty_name": "Light 2", "type": "switch" },
+		{ "name": "light3", "pretty_name": "Light 3", "type": "switch" }
+	]
+}
---
+++ lightcontrol.js
@@ -0,0 +1,38 @@
+var prefix = "";
+
+var client = new Messaging.Client(broker["hostname"], broker["port"], "clientId");
+client.onConnectionLost = onConnectionLost;
+client.setCredentials
+client.connect({onSuccess:onConnect, userName: broker["user"], password: broker["password"]});
+
+$.getJSON( "devices.js", function( data ) {
+	var items = [];
+	prefix = data.prefix
+	
+	$.each( data.devices, function( key, val ) {
+		items.push( "<li>" + val["pretty_name"] + " [<a href=\"javascript:switchOn('" + val["name"] + "');\">On</a>|<a href=\"javascript:switchOff('" + val["name"] + "');\">Off</a>]</li>" );
+	});
+	
+	$("#devices_list").html(items.join( "" ));
+});
+
+function onConnect() {
+	// Once a connection has been made, make a subscription and send a message.
+	console.log("connected");
+};
+function onConnectionLost(responseObject) {
+	if (responseObject.errorCode !== 0)
+		console.log("onConnectionLost:"+responseObject.errorMessage);
+};
+
+function switchOn(name) {
+	var message = new Messaging.Message("1");
+	message.destinationName = "mumalab/room/lights/" + name + "/state";
+	client.send(message); 
+}
+
+function switchOff(name) {
+	message = new Messaging.Message("0");
+	message.destinationName = "mumalab/room/lights/" + name + "/state";
+	client.send(message); 
+}
